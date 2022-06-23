fd208e9d56939784a5b3bd7a92bb67944da8db99
Jefferson Grassine <jeffersongrassine@gmail.com>
2022-06-23 12:25:07 -0300

    First Commit

---
+++ resources/js/Pages/Agenda/Books.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <h1>Calendário</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'Books',
+
+    components: {
+        Fullcalendar
+    },
+
+    data () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="css" scoped>
+
+</style>
---
+++ resources/js/Pages/Components/Calendar.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import FullCalendar from '@fullcalendar/vue'
+import dayGrid from '@fullcalendar/daygrid'
+import 
+
+export default {
+    name:'Calendar',
+
+    data () {
+        
+
+        return {}
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
